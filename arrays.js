b398f8cb569fcd4eab690d2c2bf24b94282830fa
EWoodard927 <EWoodard27@gmail.com>
2026-06-24 11:12:00 -0500

    I console logged everything

--- arrays.js
+++ arrays.js
@@ -2,19 +2,19 @@
 
 // 1. Update the variable songList so that it is an array with at least 3 song titles.
 let songList = ["Jane", "Married in a Year", "The World's Smallest Violin"];
-console.log();
+console.log(songList);
 
 // 2. Update the variable studentList so that it is an array with 4 Code Nation students (including yourself) in your class.
 let studentList = ["Kendall", "Miri", "Alex", "Derrick"];
-console.log();
+console.log(studentList);
 
 // 3. Update the variable teacherList so that it is an array with all the Code Nation adults in your class.
 let teacherList = ["Tyoshi", "Jack", "Tanay", "Maggie"];
-console.log();
+console.log(teacherList);
 
 // 4. Update the variable familyAges so that it is an array with a list of ages (numbers) of your family members.
 let familyAges = [14, 17, 46, 46];
-console.log();
+console.log(familyAges);
 
 //PAUSE HERE
 
@@ -22,19 +22,19 @@ console.log();
 
 // 5. Update the variable favSong to retrieve the array element of your favorite song in the songList array (Task 1). 
 let favSong = songList[2];
-console.log();
+console.log(favSong);
 
 // 6. Update the variable bestStudent to retrieve your name from the classList array (Task 2).
 let bestStudent = studentList[0];
-console.log();
+console.log(bestStudent);
 
 // 7. Update the variable tallestTeacher to retrieve the array element of the tallest teacher in the teacherList array (Task 3).
 let tallestTeacher = teacherList[2];
-console.log();
+console.log(tallestTeacher);
 
 // 8. Update the variable oldestMemeber to retrieve the array element of the oldest member of your family in the familyAges array. (Task 4)
 let oldestMember = familyAges[3];
-console.log();
+console.log(oldestMember);
 
 // Done Early?
 //  - On your own, create an array that combines both the class list and the teacher list without rewriting the names of students and teachers.
@@ -42,4 +42,5 @@ console.log();
 
 let catapultList = [...studentList, ...teacherList];
 let choice = catapultList[5];
+console.log(catapultList);
 
